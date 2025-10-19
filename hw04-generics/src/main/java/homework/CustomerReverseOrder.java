c8ae299c20fa7a0b8e41f6321adbee52e8a8e6f4
nafelitsyn <kam.06@mail.ru>
2025-10-19 23:45:36 +0300

    #task_2.2 * Modifying CustomerReverseOrder

--- hw04-generics/src/main/java/homework/CustomerReverseOrder.java
+++ hw04-generics/src/main/java/homework/CustomerReverseOrder.java
@@ -1,10 +1,10 @@
 package homework;
 
 import java.util.Deque;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 
 public class CustomerReverseOrder {
-    Deque<Customer> stackCustomers = new LinkedList<>();
+    Deque<Customer> stackCustomers = new ArrayDeque<>();
 
     public void add(Customer customer) {
         stackCustomers.add(customer);
