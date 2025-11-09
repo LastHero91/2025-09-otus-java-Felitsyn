11100d70109d8017353569e0878de68b907fbac5
nafelitsyn <kam.06@mail.ru>
2025-11-09 21:46:26 +0300

    #hw10-byteCodes * Try to implement AOP

---
+++ hw10-byteCodes/src/main/java/ru/otus/Demo.java
@@ -0,0 +1,11 @@
+package ru.otus;
+
+public class Demo {
+    public static void main(String[] args) {
+        TestLoggingInterface testLogging = Ioc.createTestLogging();
+        testLogging.calculation();
+        testLogging.calculation(6);
+        testLogging.calculation(7, 7);
+        testLogging.calculation(9, 9, "tstStr");
+    }
+}
