120dbf2573e0be5bbbd2343863a26d311a3f2902
nafelitsyn <kam.06@mail.ru>
2025-11-13 22:38:15 +0300

    #task_5.2 * Fixed based on comments

--- hw10-byteCodes/src/main/java/ru/otus/Ioc.java
+++ hw10-byteCodes/src/main/java/ru/otus/Ioc.java
@@ -3,8 +3,8 @@ package ru.otus;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.util.Arrays;
-import java.util.Objects;
+import java.util.*;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,46 +21,32 @@ class Ioc {
 
     private static class HWInvocationHandler implements InvocationHandler {
         private final TestLoggingInterface testLoggingClass;
-        private final Method[] classMethods;
+        private final List<String> methods = new ArrayList<>();
 
         HWInvocationHandler(TestLogging testLoggingClass) {
             this.testLoggingClass = testLoggingClass;
-            this.classMethods = testLoggingClass.getClass().getMethods();
+
+            for (var method : testLoggingClass.getClass().getMethods()) {
+                if (method.isAnnotationPresent(Log.class)) {
+                    methods.add(method.getName() + Arrays.asList(method.getParameterTypes()));
+                }
+            }
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            // Типы аргументов метода у интерфейса
-            var parameterTypesInterface = method.getParameterTypes();
-            // Массив значений аргументов метода у интерфейса
             var argsStr = args != null
                     ? Arrays.stream(args)
                         .map(obj -> Objects.toString(obj, "null"))
                         .toArray(String[]::new)
                     : new String[]{""};
 
-            // Обходим все методы экземпляра класса
-            for (var objMethod : classMethods){
-                // Типы аргументов метода экземпляра класса
-                var parameterTypesClass = objMethod.getParameterTypes();
-                // Проверяем:
-                if (objMethod.getAnnotation(Log.class) != null &&                 // у метода объекта есть аннотация Log
-                        objMethod.getName().equals(method.getName()) &&                         // имена методов объекта и интерфейса совпадают
-                        Arrays.equals(parameterTypesClass, parameterTypesInterface)) {   // массивы аргументов методов объекта и интерфейса совпадает
-                    logger.info("executed method: {}, param: {}",
-                            method.getName(), String.join(", ",argsStr));    // логируем имя метода и перечисление аргументов метода через запятую
-
-                    // Лог зачем мне методы объекта класса, а не аргумент method метода invoke
-                    logger.info("Interface method annotation:{};",
-                            method.getAnnotation(Log.class));
-                    logger.info("Object method annotation:{};",
-                            objMethod.getAnnotation(Log.class));
-                    // Лог для удобства просмотра
-                    logger.info("---------------------------------------------");
+            if (methods.contains(method.getName() + Arrays.asList(method.getParameterTypes()))
+                    || method.isAnnotationPresent(Log.class)) {
+                logger.info("executed method: {}, param: {}",
+                        method.getName(), String.join(", ",argsStr));
                 }
-            }
 
-            // Вызываем "оригинальный" метод
             return method.invoke(testLoggingClass, args);
         }
 
